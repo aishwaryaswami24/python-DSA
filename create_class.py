fd26d65f61343807cd25a6ce036f5a68a5f73d77
aishwarya <swamiaishwarya24@gmail.com>
2024-08-28 20:07:13 +0530

    class

--- create_class.py
+++ create_class.py
@@ -1,18 +1,24 @@
+
+#Car is a class
 class Car:
     def __init__(self,model,color): #constructor
         self.model=model
         self.color=color
 
 
-
+#mycar is a object
 mycar= Car('BMW','blue')
 print(mycar.color,mycar.model)
 
-#without constructor
+
+
+
+#without constructor python calls default one which doesn't inistializes object attributes
 class Car:
     pass
 
 mycar=Car()
 
+#need to pass manually attributes values
 mycar.color='blue'
 print(mycar.color)
