bbcb0c9721cd65191d8223d252a87593ea9c4c8d
aishwarya <swamiaishwarya24@gmail.com>
2024-08-28 19:56:22 +0530

    constructor

---
+++ Constructor.py
@@ -0,0 +1,13 @@
+class Car:
+
+    def __init__(self,model,colour):
+        self.model=model
+        self.colour=colour
+
+    def check_colour(self):
+        return "colour checking done"
+
+
+my_car=Car('Bullet','Black')
+
+print(my_car.check_colour())
