d2cf2970f2c11821f88e0683ad07840cf05a75cd
aishwarya <swamiaishwarya24@gmail.com>
2024-09-03 23:04:52 +0530

    identical

---
+++ identical.py
@@ -0,0 +1,10 @@
+#find out identical numbers in the given list
+A = [1, 2, 3]
+B = [1, 2, 1]
+
+
+#wrote in call
+if A[0] == B[0] or A[1] == B[1] or A[2]==B[2]:
+    print('identical')
+else:
+    print('not identical')
