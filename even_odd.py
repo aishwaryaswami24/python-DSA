d770ec8d947ab5a8875ff9d46b08545971dd6910
aishwarya <swamiaishwarya24@gmail.com>
2024-09-04 21:19:41 +0530

    Even & Odd

--- even_odd.py
+++ even_odd.py
@@ -1,5 +1,19 @@
+# def main():
+#     x = int(input("enter number: "))
+#     if is_even(x):
+#         print('Even')
+#     else:
+#         print('Odd')
+#
+#
+# def is_even(n):
+#     return n%2==0
+#
+#
+# main()
+
 def main():
-    x = int(input("enter number: "))
+    x=int(input('what is x: '))
     if is_even(x):
         print('Even')
     else:
@@ -7,7 +21,8 @@ def main():
 
 
 def is_even(n):
-    return n%2==0
-
-
+    if n % 2 == 0:
+        return True
+    else:
+        return False
 main()
