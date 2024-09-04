f89856058c85964a51752ef8b4dd30845fde22d9
aishwarya <swamiaishwarya24@gmail.com>
2024-09-04 23:53:28 +0530

    print

--- print.py
+++ print.py
@@ -1,9 +1,19 @@
+# x=int(input('Enter value1: '))
+# y=int(input('Enter value2: '))
+#
+# if x>y:
+#     print('x is greter than y')
+# elif x<y:
+#     print('x is less than y')
+# else:
+#     print('x is equal to y')
+#
+
+#2nd way:
 x=int(input('Enter value1: '))
 y=int(input('Enter value2: '))
 
-if x>y:
-    print('x is greter than y')
-elif x<y:
-    print('x is less than y')
+if x>y or x<y:
+    print('x is not equal to y')
 else:
     print('x is equal to y')
