7dfc8d709fd0b7ec088ec3781484b4b1cd13b0ae
aishwarya <swamiaishwarya24@gmail.com>
2024-09-06 19:37:43 +0530

    if elif

--- if_elif.py
+++ if_elif.py
@@ -1,7 +1,20 @@
-name=input("what is your name: ")
+#1st way
+# name=input("what is your name: ")
+# if name=="Aishwarya":
+#     print("TCS")
+# elif name=="Shubham":
+#     print("UST")
+# else:
+#     print("DB empty")
+
+#2nd way
+
+name=input('what is your name: ')
 if name=="Aishwarya":
     print("TCS")
 elif name=="Shubham":
+    print("UST")
+elif name=="Ratndeep":
     print("UST")
 else:
     print("DB empty")
