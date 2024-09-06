576261b42626b7eee7fd56d14e936ff62bd5c4f1
aishwarya <swamiaishwarya24@gmail.com>
2024-09-06 19:49:46 +0530

    match

--- match_statement_3.10.py
+++ match_statement_3.10.py
@@ -1,13 +1,31 @@
-name = input("What's your name? ")
+# name = input("What's your name? ")
+#
+#   match name:
+#       case "Harry" | "Hermione" | "Ron":
+#           print("Gryffindor")
+#       case "Draco":
+#           print("Slytherin")
+#       case _:
+#           print("Who?")
 
-  match name:
-      case "Harry" | "Hermione" | "Ron":
-          print("Gryffindor")
-      case "Draco":
-          print("Slytherin")
-      case _:
-          print("Who?")
 #getting error bez it availble in 3.10 python version
 #  match name:
 #           ^
 # SyntaxError: invalid syntax
+
+#match is availble in python 3.10 version
+# Input from the user
+name = input("What's your name? ")
+
+# Match case structure
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
