db89709326381af2aaefb75022175c7d8e3b1c2a
aishwarya <swamiaishwarya24@gmail.com>
2024-09-10 23:03:27 +0530

    remove duplicates from list 1.using set 2.using dict.fromkeys 3.using for loop

--- duplicate.py
+++ duplicate.py
@@ -14,3 +14,12 @@ lst1=[1,2,3,4,4,9,9,9,4]
 remove_duplicates=list(dict.fromkeys(lst1))
 print(remove_duplicates)
 
+#using a for loop
+lst2=[1,1,1,2,3,4,4,4,5,6,7,9,0,0,0]
+empty_lst=[]
+for i in lst2:
+    if i not in empty_lst:
+        empty_lst.append(i)
+print(f'Previous list: ',lst2)
+print(f'Current list: ',empty_lst)
+
