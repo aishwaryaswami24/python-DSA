4e31cde8946f91426db3b037403881f1b674a7d3
aishwarya <swamiaishwarya24@gmail.com>
2024-09-10 22:53:31 +0530

    remove duplicates from list

--- duplicate.py
+++ duplicate.py
@@ -1,8 +1,11 @@
 lst=[1,1,1,1,3,4,5,5,6,7,0,4]
 #list converted into set
 unique_no=set(lst)
+print(unique_no)
 
 #set convert to list if you want to
-unique_no=list(set(lst)
+unique_no=list(set(lst))
+print(unique_no)
 
-print(f"Duplicates values removed from the list",unique_no)
+print(f"Duplicates values removed from the list")
+
