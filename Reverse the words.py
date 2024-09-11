75f4e49697bcd73f1875087cfeee0f35fcd002e9
aishwarya <swamiaishwarya24@gmail.com>
2024-09-11 11:13:44 +0530

    reverse the words return ' ' .join(s.spilt()[::-1])

--- Reverse the words.py
+++ Reverse the words.py
@@ -1,6 +1,7 @@
 
 def reverse_string(s):
-   return ' ' .join(s.split()[::-1])
+   return ' ' .join(s.split()[::-1]) #with join
+   return s.split()[::-1] #without join
 
 s='Hello World'
 print(reverse_string(s))
