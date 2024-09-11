50606c1f99345d2c493d266f484bfd9b26d85914
aishwarya <swamiaishwarya24@gmail.com>
2024-09-11 10:41:18 +0530

    reverse string with slicing [::-1]

--- reverse_string.py
+++ reverse_string.py
@@ -1,14 +1,25 @@
 # Write a Python program to reverse a string.
 # Function to reverse a string
-def reverse_string(input_string):
-    # Reversing the string using slicing
-    return input_string[::-1]
+# def reverse_string(input_string):
+#     # Reversing the string using slicing
+#     return input_string[::-1]
+#
+# # Input string
+# string = "Hello, World!"
+# # Calling the function
+# reversed_string = reverse_string(string)
+#
+# # Output the reversed string
+# print("Original String:", string)
+# print("Reversed String:", reversed_string)
 
-# Input string
-string = "Hello, World!"
-# Calling the function
-reversed_string = reverse_string(string)
 
-# Output the reversed string
-print("Original String:", string)
-print("Reversed String:", reversed_string)
+#simple hello reverse code
+
+def reverse_string(s):
+    return s[::-1]
+#Slicing with [::-1] reverses the string by iterating backward
+
+
+s='hello'
+print(reverse_string(s))
