50a983e20b6edf3bb5ca39639da1ac9d3b7ffa98
aishwarya <swamiaishwarya24@gmail.com>
2024-09-12 23:10:29 +0530

    identical

--- practice.py
+++ practice.py
@@ -1,4 +1,13 @@
+#json
 import json
 json_data = '{"id": 1234, "name": "Aishwarya"}'
 data=json.loads(json_data)
 print(data['id'],data['name'])
+
+#identical
+A=[1,1,2,3]
+B=[3,2,2,2,1]
+C=list((set(A)&set(B)))
+print(C)
+
+
