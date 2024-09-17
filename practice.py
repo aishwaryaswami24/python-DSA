e1381436df69162773c3e847989a3c1e887d3da3
aishwarya <swamiaishwarya24@gmail.com>
2024-09-17 22:53:11 +0530

    print

--- practice.py
+++ practice.py
@@ -230,7 +230,8 @@ print('name is:',data['name'])
 #print
 name=input('what is your name')
 print('Hello World',name)
-print('Hello Worlds'+name)
+print('Hello Worlds '+name)
+print(f'Hello World {name}')
 
 
 
