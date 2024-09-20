67c3ea177c3c5488d9041dabcba48a36cfb7342e
aishwarya <swamiaishwarya24@gmail.com>
2024-09-20 12:20:36 +0530

    TypeError:concatenate str(not 'bool')

--- python_practice.py
+++ python_practice.py
@@ -1,19 +1,23 @@
-def func():
-    def nested():
-        global x
-        x=1
-x=2
-func()
-print(x)
+# def func():
+#     def nested():
+#         global x
+#         x=1
+# x=2
+# func()
+# print(x)
+#
+# #str
+# x='aish'
+# y=24
+# print(x+str(y))
+# print(x,str(y))
+#
+# #add int and str:TypeError
+# x='abc'
+# y=5
+# print(x+y)
 
-#str
-x='aish'
-y=24
-print(x+str(y))
-print(x,str(y))
-
-#add int and str:TypeError
-x='abc'
-y=5
+#boolean True & False
+x='xyz'
+y=True
 print(x+y)
-
