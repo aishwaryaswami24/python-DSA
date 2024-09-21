6d1b93784194078bdbbaa060ff51ffc981fa3615
aishwarya <swamiaishwarya24@gmail.com>
2024-09-21 23:47:09 +0530

    factorial

--- factorial.py
+++ factorial.py
@@ -2,6 +2,7 @@ from datetime import datetime
 now=datetime.now()
 print(now)
 
+#Python’s math library provides a built-in function factorial() that can compute the factorial of a number.
 import math
 num=int(input('Enter the number:'))
-print('Factorial of {num} is:{math.factorial(num)}')
+print(f'Factorial of {num} is:{math.factorial(num)}')
