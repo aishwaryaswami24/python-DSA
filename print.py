2cc569ccc52785877c76147e2443ece035b82484
aishwarya <swamiaishwarya24@gmail.com>
2024-09-22 13:11:23 +0530

    // is a floor division operator, division rounded down

--- print.py
+++ print.py
@@ -39,30 +39,49 @@
 #     print('end')
 
 #replace
-str='11AISH1'
-print(str.replace('1','One'))
-print(str.replace('1','One',0))
-print(str.replace('1','One',1))
+# str='11AISH1'
+# print(str.replace('1','One'))
+# print(str.replace('1','One',0))
+# print(str.replace('1','One',1))
 #here we have provided count 0 then it will not replace 1 by One
 #if you give count=1 then it will replace only one occrance
 
 #find
-text='har war bar'
-print(text.find('bar')) #it returns starting index number
-i=text.find('bar',9) #Start searching from index 9
-print(i)
+# text='har war bar'
+# print(text.find('bar')) #it returns starting index number
+# i=text.find('bar',9) #Start searching from index 9
+# print(i)
 #it returns starting index number
 #find() string method return the index of the first occurrence of a substring
 
 #_doc_
-def text():
-    '''doc'''
-    '''hello'''
-print(text.__doc__)
+# def text():
+#     '''doc'''
+#     '''hello'''
+# print(text.__doc__)
 
 
-print('Hello',end='111')
-print('Hello')
+# print('Hello',end='111')
+# print('Hello')
 #1st print did not end on new line it remains on same line so 2nd print continued with the same line
 
-#syntax error
+#syntax error/#recursion error
+def func():
+
+    func()
+
+#slicing
+t='AaBbCcDd'
+print(t[::2])
+print(t[1::2])
+# t[::2] picks every second character starting from index 0, giving "ABC".
+# t[1::2] picks every second character starting from index 1, giving "aBd".
+
+#t[start:stop:step]
+t='12345'
+print(t[1::-1])
+
+
+#// is a floor division operator, division rounded down
+a=5//3
+print(a)
