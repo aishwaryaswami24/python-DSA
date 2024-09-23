f755fd37335ca92ed74b3884aa187436e5139f2e
aishwarya <swamiaishwarya24@gmail.com>
2024-09-23 21:21:28 +0530

    factorial

--- factorial.py
+++ factorial.py
@@ -1,15 +1,19 @@
-from datetime import datetime
-now=datetime.now()
-print(now)
+# from datetime import datetime
+# now=datetime.now()
+# print(now)
+#
+# #time
+# import time
+# tme=time.strftime('%H:%M:%S',time.localtime())
+# print(tme)
+#
+# #Python’s math library provides a built-in function factorial() that can compute the factorial of a number.
+# import math
+# num=int(input('Enter the number:'))
+# print(f'Factorial of {num} is:{math.factorial(num)}')
+#
+# #it will raise a ValueError if you input a negative number.
 
-#time
-import time
-tme=time.strftime('%H:%M:%S',time.localtime())
-print(tme)
 
-#Python’s math library provides a built-in function factorial() that can compute the factorial of a number.
-import math
-num=int(input('Enter the number:'))
-print(f'Factorial of {num} is:{math.factorial(num)}')
+#factorial 2nd way
 
-#it will raise a ValueError if you input a negative number.
