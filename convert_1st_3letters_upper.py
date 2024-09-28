8537a5baea6702ea0e9c6652cae48cf78426c188
aishwarya <swamiaishwarya24@gmail.com>
2024-09-28 17:37:12 +0530

    upper() , lower() and slicing

--- convert_1st_3letters_upper.py
+++ convert_1st_3letters_upper.py
@@ -1,6 +1,14 @@
+#convert 1st 3 letters to upper and rest lower
+#aishwarya swami
+#012345678 9 101112131415 -> index numering
+#string[start:end]  ->
+'''start: The starting index (inclusive) of the slice.
+end: The ending index (exclusive) of the slice.
+'''
+'''If you leave out start, it defaults to 0 (the beginning of the string). 
+If you leave out end, it defaults to the length of the string (the end of the string).'''
 
 
 str='aishwarya swami'
-
 modified_str=str[:3].upper()+str[3:].lower()
 print(modified_str)
