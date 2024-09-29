6533f203734b8071699f933328fecc79214d5d1d
aishwarya <swamiaishwarya24@gmail.com>
2024-09-29 07:21:28 +0530

    s[:3].upper()+s[3:].lower()

--- convert_1st_3letters_upper.py
+++ convert_1st_3letters_upper.py
@@ -1,11 +1,27 @@
-#convert 1st 3 letters to upper and rest lower
-#aishwarya swami
-#012345678 9 101112131415 -> index numering
-#string[start:end]  ->
-'''start: The starting index (inclusive) of the slice.
-end: The ending index (exclusive) of the slice.
-'''
-'''If you leave out start, it defaults to 0 (the beginning of the string). 
-If you leave out end, it defaults to the length of the string (the end of the string).'''
+# #convert 1st 3 letters to upper and rest lower
+# #aishwarya swami
+# #012345678 9 101112131415 -> index numering
+# #string[start:end]  ->
+# '''start: The starting index (inclusive) of the slice.
+# end: The ending index (exclusive) of the slice.
+# '''
+# '''If you leave out start, it defaults to 0 (the beginning of the string).
+# If you leave out end, it defaults to the length of the string (the end of the string).'''
+#
+#
+
+
+
+s='aishwarya swami'
+convert=s[:3].upper()+s[3:].lower()
+print(convert)
+
+
+
+
+
+
+
+
 
 
