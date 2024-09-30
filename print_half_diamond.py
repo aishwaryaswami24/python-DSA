d2338d18d1569ff7b963e00eb8128a997acdfeb3
aishwarya <swamiaishwarya24@gmail.com>
2024-09-30 20:40:27 +0530

    half diamond

--- print_half_diamond.py
+++ print_half_diamond.py
@@ -1,26 +1,26 @@
-n=5
-
-i=1
-while i <= n:
-    print('*' * i)
-    i += 1
-
-i=n-1
-while i > 0:
-    print('*' * i)
-    i -= 1
+# n=5
+#
+# i=1
+# while i <= n:
+#     print('*' * i)
+#     i += 1
+#
+# i=n-1
+# while i > 0:
+#     print('*' * i)
+#     i -= 1
 
 """print half diamond """
 n=5
 i=1
-while i>=n:
+while i<=n:
     print('*'*i)
     i +=1
 
 i=n-1
 i=0
 
-while i<n:
+while i>n:
     print('*'*i)
     i -=1
 
